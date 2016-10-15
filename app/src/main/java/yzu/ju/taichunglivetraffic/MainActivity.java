670f58dfea79e2cd4e6674888d34984f6c3072fd
ju1101 <yujuchen1101@gmail.com>
2016-10-15 15:10:06 +0800

    e

---
+++ app/src/main/java/yzu/ju/taichunglivetraffic/MainActivity.java
@@ -0,0 +1,51 @@
+package yzu.ju.taichunglivetraffic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    public void buttonClick(View view){
+        // 啟動地圖元件用的Intent物件
+        Intent intentMap = new Intent(this, MapsActivity.class);
+        // 啟動地圖元件
+        startActivity(intentMap);
+        MainActivity.this.finish();
+    }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    msg += "Click edit";
+                    break;
+                case R.id.action_share:
+                    msg += "Click share";
+                    break;
+                case R.id.action_settings:
+                    msg += "Click setting";
+                    break;
+            }
+
+            if(!msg.equals("")) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+}
